5664ff7ff31b560b04d124ca0ea87c337935e452
vidyarthi <vidyarthi004@gmail.com>
2025-06-19 16:56:36 +0530

    add db

--- db.php
+++ db.php
@@ -1,6 +1,12 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "encodingo_certificates");
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
+
+$conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 ?>
